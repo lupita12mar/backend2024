ccc4ed23b0a2ddabdad60fbe83b3d67f82d6489c
Guadalupe Martinez Ignacio <lupitajosmar1227@gmail.com>
2024-10-08 11:58:37 -0600

    modificaciones

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -15,17 +15,30 @@ const usuarios=[
         email: "marly.1212@gmail.com"
     },
 ];
+
+
 app.get("/usuarios", (req, res)=> {
     
     res.status(200).send({usuarios});
 })//obtener informacion
 
-app.get("/usuarios/:id", (req, res)=>{
-const id = req.params;
-const usuario= usuarios.find((usuario)=>usuario.id=== +id);
-    
-    res.status(200).send("probando");
-});
+
+app.get("/usuarios/:id",(req, res)=>{
+    const {id}=req.params;
+    //const params = req.params;
+    //console.log(params);
+    //console.log(typeof(+id));
+    const usuario= usuarios.find((usuario)=>usuario.id === +id);
+   if(usuario===undefined){
+    res.status(404).send({error:`el usuario con id ${id} no existe`});
+    return;
+   };
+    res.status(200).send(usuario);
+    //res.status(200).send("probando");
+
+});//end point
+
+
 app.listen(3000, ()=>{
     console.log("Servidor corriendo en http://localhost:3000")
 });
