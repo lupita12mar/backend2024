60e2d534091d660dd2af3110c56817e709f5af35
Guadalupe Martinez Ignacio <lupitajosmar1227@gmail.com>
2024-12-04 16:32:12 -0600

    clase 04/12/2024

--- pokemon_game/src/routes/users.js
+++ pokemon_game/src/routes/users.js
@@ -1,13 +1,12 @@
-const {Router} = require('express');
-const{getAllUsers} = require('../controllers/users');
+const { Router } = require('express');
+const { getAllUsers, createUser, getUser, updateUser, destroyUser } = require('../controllers/users');
 
 const router = Router();
 
 router.get('/', getAllUsers);
-router.get('/:id');
-router.post('/');
-router.put('/:id');
-router.delete('/:id');
-
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', destroyUser);
 
 module.exports = router;
